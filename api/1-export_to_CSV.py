d30df01c14745b3e31575345d87ce59ad71dbd07
Jeremy Mitts <jermitts@gmail.com>
2024-07-12 21:33:08 -0500

    project 2076 task 1 but better this time (using csv)

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,42 +1,45 @@
 #!/usr/bin/python3
 """ api project task 1 """
 
+import csv
 import json
 import sys
 import urllib.request
 
+
 base_url = "https://jsonplaceholder.typicode.com/"
 
 
+def fetch_write(empoyee_id):
+    """ get the stuff and do the thing """
+    url = base_url + "users/{}".format(employee_id)
+    with urllib.request.urlopen(url) as response:
+        user = json.loads(response.read())
+
+    url = base_url + "todos?userId={}".format(employee_id)
+    with urllib.request.urlopen(url) as response:
+        todos = json.loads(response.read())
+
+    todos_out = []
+    for todo in todos:
+        task = {}
+        task["user_id"] = user["id"]
+        task["username"] = user["username"]
+        task["completed"] = todo["completed"]
+        task["title"] = todo["title"]
+        todos_out.append(task)
+
+    with open(f"{user['id']}.csv", "w") as csvfile:
+        fieldnames = ["user_id", "username", "completed", "title"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
+
+        for todo in todos_out:
+            writer.writerow(todo)
+
+
 if __name__ == "__main__":
-
-    out = ""
-
     if len(sys.argv) > 1:
-        employee_id = int(sys.argv[1])
-        url = base_url + "users/{}".format(employee_id)
-        with urllib.request.urlopen(url) as response:
-            user_data = response.read()
-        user = json.loads(user_data)
-        employee_name = user["username"]
-
-        url = base_url + "todos?userId={}".format(employee_id)
-        with urllib.request.urlopen(url) as response:
-            todo_data = response.read()
-        #  parse and load JSON from todos_data into todos
-        todos = json.loads(todo_data)
-
-        for todo in todos:
-            completed = todo['completed']
-            title = todo['title']
-            out += f'"{employee_id}",' + \
-                f'"{employee_name}",' + \
-                f'"{completed}",' + \
-                f'"{title}"\n'
-
-        #  write the out to a file named employee_id.csv
-        with open(f"{employee_id}.csv", "w") as f:
-            f.write(out)
-
+        for employee_id in sys.argv[1:]:
+            fetch_write(int(employee_id))
     else:
         print("Please provide an employee ID as a command-line argument.")
