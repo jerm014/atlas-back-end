1110e1347c283c0cc1a5aaf14fead151d80a3655
Jeremy Mitts <jermitts@gmail.com>
2024-07-12 20:54:34 -0500

    project 2076 task 3 but better this time

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -9,19 +9,8 @@ base_url = "https://jsonplaceholder.typicode.com/"
 
 
 def fetch_write():
-    """ I am well aware that I should use dictionaries
-    and stuff and then convert the stuff to json using
-    the json library, but this way makes more sense to
-    me when reading it and is faster to write for me
-    because of my limited experience with the json lib-
-    ary and I'm kind of in a hurry so I feel like, for
-    me, this would be more real-world than taking the
-    time to learn a brand new thing when you can hack
-    it out and get the same result. No one is paying
-    extra for pretty and perfect code, they just want
-    it to work, and my employeer wanted this task done
-    last week and won't stop breathing down my neck so
-    what do you expect, really?"""
+    """ get the stuff and do the thing """
+    out = {}
 
     with urllib.request.urlopen(base_url + "users") as response:
         employees_data = response.read()
@@ -30,40 +19,24 @@ def fetch_write():
         todo_data = response.read()
     todos = json.loads(todo_data)
 
-    out = '{'
-    not_first_employee = False
-
     for employee in employees:
-        if not_first_employee:
-            out += ', '
-        else:
-            not_first_employee = True
-
         employee_id = employee["id"]
         employee_username = employee["username"]
 
-        out += f'"{employee_id}": '
-        out += '['
-        not_first_task = False
+        tasks = []
         for todo in todos:
             if todo['userId'] != employee_id:
                 continue
-            completed = todo['completed']
-            title = todo['title']
-            if not_first_task:
-                out += ', '
-            else:
-                not_first_task = True
-            out += '{'
-            out += f'"task": "{title}", '
-            out += f'"completed": {str(completed).lower()}, '
-            out += f'"username": "{employee_username}"'
-            out += '}'
-        out += ']'
-    out += '}'
+            task = {}
+            task['task'] = todo['title']
+            task['completed'] = todo['completed']
+            task['title'] = todo['title']
+            task['username'] = employee_username
+            tasks.append(task)
+        out[employee_id] = tasks
 
     with open(f"todo_all_employees.json", "w") as f:
-        f.write(out)
+        json.dump(out, f)
 
 
 def do_it_with_github():
